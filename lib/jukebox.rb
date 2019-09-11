b5d90dc2f0fb647c78062e5c5a513462e726e093
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-11 17:27:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,15 +1,15 @@
-# songs = [
-#   "Phoenix - 1901",
-#   "Tokyo Police Club - Wait Up",
-#   "Sufjan Stevens - Too Much",
-#   "The Naked and the Famous - Young Blood",
-#   "(Far From) Home - Tiga",
-#   "The Cults - Abducted",
-#   "Phoenix - Consolation Prizes",
-#   "Harry Chapin - Cats in the Cradle",
-#   "Amos Lee - Keep It Loose, Keep It Tight"
-# ]
-#
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
 def help
 puts "I accept the following commands:"
 puts "- help : displays this help message"
@@ -38,54 +38,34 @@ def play(songs)
  end
 end
 
-
-
-def play(songs)
-  puts "Please enter a song name or number:"
-  song_to_play = gets.chomp
-  if songs.include?(song_to_play)
-    puts "Playing #{song_to_play}"
-  else if (1...9).to_a.include?(song_to_play.to_i)
-    puts "Playing #{songs[song_to_play.to_i - 1]}"
-  else
-    puts "Invalid input, please try again"
-  end
-end
+def list(songs)
+songs.each_with_index {|song,index|index+=1
+   puts "#{index}. ""#{song}"
+ }
 end
 
 def exit_jukebox
   puts "Goodbye"
 end
 
-
-# def list(songs)
-# songs.each_with_index {|song,index|index+=1
-#    puts "#{index}. ""#{song}"
-#  }
-# end
-#
-# def exit_jukebox
-#   puts "Goodbye"
-# end
-#
-# def run(songs)
-# stop="exit"
-# help_user="help"
-# list_songs="list"
-# play_music="play"
-#   puts "Please enter a command:"
-# user_input=gets.strip
-# case user_input
-# when stop
-#   exit_jukebox
-# when help_user
-#   help
-# when list_songs
-#   list(songs)
-# when play_music
-# play(songs[0])
-# end
-# end
+def run(songs)
+stop="exit"
+help_user="help"
+list_songs="list"
+play_music="play"
+  puts "Please enter a command:"
+user_input=gets.strip
+case user_input
+when stop
+  exit_jukebox
+when help_user
+  help
+when list_songs
+  list(songs)
+when play_music
+play(songs[0])
+end
+end
 
 
 
