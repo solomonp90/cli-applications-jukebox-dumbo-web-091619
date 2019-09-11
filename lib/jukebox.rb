ee478e68a8bffa48a55c157ddbfba8ab738befbd
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-11 17:46:44 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,7 +17,7 @@ puts "- list : displays a list of songs you can play"
 puts "- play : lets you choose a song to play"
 puts "- exit : exits this program"
 end
-#
+
 def play(songs)
  number_not_listed="12323"
  full_name="Phoenix - 1901"
@@ -66,93 +66,3 @@ when play_music
 play(songs[0])
 end
 end
-
-
-
-
-
-
-# def run(songs)
-# stop="exit"
-# help_user="help"
-# list_songs="list"
-# play_music="play"
-# puts "Please enter a command:"
-# user_input=gets.strip
-#
-# case user_input
-# when stop
-#   exit_jukebox
-# when help_user
-#   help
-# when list_songs
-#   list(songs)
-# when play_music
-#   play(songs[0])
-# end
-#
-# end
-
-# def run(songs)
-# help="help"
-# play="play"
-# list="list"
-# exit_jukebox="exit"
-# puts "Please enter a command:"
-# user_input=gets.strip
-# case user_input
-# when help
-#  help
-# when play
-#   play
-# when list
-#   list
-# when exit_jukebox
-#   exit_jukebox
-# end
-# end
-   #
-   #
-  #  def list(songs)
-  #    songs.each_with_index do |song_name, i|
-  #      puts "#{i+1}. #{song_name}"
-  #    end
-  #  end
-   #
-  #  def play(songs)
-  #    puts "Please enter a song name or number:"
-  #    song_to_play = gets.chomp
-  #    if songs.include?(song_to_play)
-  #      puts "Playing #{song_to_play}"
-  #    else if (1...9).to_a.include?(song_to_play.to_i)
-  #      puts "Playing #{songs[song_to_play.to_i - 1]}"
-  #    else
-  #      puts "Invalid input, please try again"
-  #    end
-  #  end
-  #  end
-   #
-  #  def exit_jukebox
-  #    puts "Goodbye"
-  #  end
-
-  # def run(songs)
-  #     input = ""
-  #     while input
-  #     puts "Please enter a command:"
-  #     input = gets.downcase.chomp
-  #     case input
-  #     when "list"
-  #       list(songs)
-  #     when "help"
-  #       help
-  #     when "play"
-  #       play(songs)
-  #     when "exit"
-  #     exit_jukebox
-  #     break
-  #   else
-  #     help
-  #   end
-  # end
-  # end
